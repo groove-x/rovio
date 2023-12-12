759aa2413d196b454e4fde49bf2957922e499559
Kohei Takada <kohei.takada@groove-x.com>
2023-12-12 20:40:52 +0900

    Refactor.

--- src/Patch.cu
+++ src/Patch.cu
@@ -7,30 +7,6 @@
 #include "rovio/FeatureCoordinates.hpp"
 #include "rovio/Patch.cuh"
 
-#if 0
-__global__ void extractPatchFromImageKernel(
-    const cv::cudev::GlobPtrSz<const uchar> d_img, float* d_patch,
-    const int patchSize, const float c_x, const float c_y,
-    const bool withBorder) {
-  int x = blockIdx.x * blockDim.x + threadIdx.x;
-  int y = blockIdx.y * blockDim.y + threadIdx.y;
-  int halfpatch_size = patchSize / 2 + (int)withBorder;
-
-  // Calculate the coordinates in the image
-  float img_x = c_x + x - halfpatch_size;
-  float img_y = c_y + y - halfpatch_size;
-
-  // Check if the thread is within the patch bounds
-  if (x < 2 * halfpatch_size && y < 2 * halfpatch_size && img_x >= 0 &&
-      img_y >= 0 && img_x < d_img.cols && img_y < d_img.rows) {
-    // Perform the patch extraction
-    int img_x_int = static_cast<int>(img_x);
-    int img_y_int = static_cast<int>(img_y);
-    d_patch[y * patchSize + x] = d_img(img_y_int, img_x_int);
-  }
-}
-#endif
-
 __global__ void extractPatchFromImageKernel(
     const cv::cudev::GlobPtrSz<const uchar> d_img, float* d_patch,
     float* d_patchWithBorder, const int patchSizeWithBorder, const float c_x,
@@ -39,43 +15,45 @@ __global__ void extractPatchFromImageKernel(
   const int refStep = d_img.step;  // Assuming step is a property of d_img
 
   // Calculate global thread ID
-  int x = blockIdx.x * blockDim.x + threadIdx.x;
-  int y = blockIdx.y * blockDim.y + threadIdx.y;
-
-  // Get pixel coordinates without border (border width = 1)
-  int x_wo_border = x - 1;
-  int y_wo_border = y - 1;
-  bool within_border = x_wo_border >= 0 && y_wo_border >= 0 &&
-                       x_wo_border < 2 * (halfpatch_size - 1) &&
-                       y_wo_border < 2 * (halfpatch_size - 1);
+  const int x = blockIdx.x * blockDim.x + threadIdx.x;
+  const int y = blockIdx.y * blockDim.y + threadIdx.y;
 
   // Check if within bounds
-  if (x < 2 * halfpatch_size && y < 2 * halfpatch_size) {
-    const int u_r = floor(c_x);
-    const int v_r = floor(c_y);
+  if (x >= 2 * halfpatch_size || y >= 2 * halfpatch_size) return;
 
-    // Compute interpolation weights
-    const float subpix_x = c_x - u_r;
-    const float subpix_y = c_y - v_r;
-    const float wTL = (1.0 - subpix_x) * (1.0 - subpix_y);
-    const float wTR = subpix_x * (1.0 - subpix_y);
-    const float wBL = (1.0 - subpix_x) * subpix_y;
-    const float wBR = subpix_x * subpix_y;
+  const int u_r = floor(c_x);
+  const int v_r = floor(c_y);
 
-    const int img_x = u_r - halfpatch_size + x;
-    const int img_y = v_r - halfpatch_size + y;
-    const uchar* img_ptr = d_img.data + img_y * refStep + img_x;
-    float* patch_ptr = d_patchWithBorder + y * patchSizeWithBorder + x;
+  // Compute interpolation weights
+  const float subpix_x = c_x - u_r;
+  const float subpix_y = c_y - v_r;
+  const float wTL = (1.0 - subpix_x) * (1.0 - subpix_y);
+  const float wTR = subpix_x * (1.0 - subpix_y);
+  const float wBL = (1.0 - subpix_x) * subpix_y;
+  const float wBR = subpix_x * subpix_y;
 
-    *patch_ptr = wTL * img_ptr[0];
-    if (subpix_x > 0) *patch_ptr += wTR * img_ptr[1];
-    if (subpix_y > 0) *patch_ptr += wBL * img_ptr[refStep];
-    if (subpix_x > 0 && subpix_y > 0) *patch_ptr += wBR * img_ptr[refStep + 1];
+  const int img_x = u_r - halfpatch_size + x;
+  const int img_y = v_r - halfpatch_size + y;
+  if (img_x < 0 || img_y < 0 || img_x >= d_img.cols || img_y >= d_img.rows)
+    return;
 
-    if (within_border) {
-      float* patch_wo_border_ptr = d_patch + y_wo_border * 2 * halfpatch_size + x_wo_border;
-      *patch_wo_border_ptr = *patch_ptr;
-    }
+  const uchar* img_ptr = d_img.data + img_y * refStep + img_x;
+  float* patch_ptr = d_patchWithBorder + y * patchSizeWithBorder + x;
+  *patch_ptr = wTL * img_ptr[0];
+  if (subpix_x > 0) *patch_ptr += wTR * img_ptr[1];
+  if (subpix_y > 0) *patch_ptr += wBL * img_ptr[refStep];
+  if (subpix_x > 0 && subpix_y > 0) *patch_ptr += wBR * img_ptr[refStep + 1];
+
+  // Get pixel coordinates without border (border width = 1)
+  const int x_wo_border = x - 1;
+  const int y_wo_border = y - 1;
+  const bool within_border = x_wo_border >= 0 && y_wo_border >= 0 &&
+                             x_wo_border < 2 * (halfpatch_size - 1) &&
+                             y_wo_border < 2 * (halfpatch_size - 1);
+  if (within_border) {
+    float* patch_wo_border_ptr =
+        d_patch + y_wo_border * 2 * halfpatch_size + x_wo_border;
+    *patch_wo_border_ptr = *patch_ptr;
   }
 }
 
@@ -87,49 +65,49 @@ __global__ void extractPatchFromImageNearIdentityWarpingKernel(
   const int refStep = d_img.step;  // Assuming step is a property of d_img
 
   // Calculate global thread ID
-  int x = blockIdx.x * blockDim.x + threadIdx.x;
-  int y = blockIdx.y * blockDim.y + threadIdx.y;
+  const int x = blockIdx.x * blockDim.x + threadIdx.x;
+  const int y = blockIdx.y * blockDim.y + threadIdx.y;
+  if (x >= 2 * halfpatch_size || y >= 2 * halfpatch_size) return;
+
+  const float dx = x - halfpatch_size + 0.5f;
+  const float dy = y - halfpatch_size + 0.5f;
+
+  const float wdx = warp_c[0][0] * dx + warp_c[0][1] * dy;
+  const float wdy = warp_c[1][0] * dx + warp_c[1][1] * dy;
+
+  const float u_pixel = c_x + wdx - 0.5f;
+  const float v_pixel = c_y + wdy - 0.5f;
+
+  const int u_r = floor(u_pixel);
+  const int v_r = floor(v_pixel);
+  if (u_r < 0 || v_r < 0 || u_r >= d_img.cols || v_r >= d_img.rows) return;
+
+  const float subpix_x = u_pixel - u_r;
+  const float subpix_y = v_pixel - v_r;
+
+  const float wTL = (1.0f - subpix_x) * (1.0f - subpix_y);
+  const float wTR = subpix_x * (1.0f - subpix_y);
+  const float wBL = (1.0f - subpix_x) * subpix_y;
+  const float wBR = subpix_x * subpix_y;
+
+  const uchar* img_ptr = d_img.data + v_r * refStep + u_r;
+  float* patch_ptr = d_patchWithBorder + y * patchSizeWithBorder + x;
+
+  *patch_ptr = wTL * img_ptr[0];
+  if (subpix_x > 0) *patch_ptr += wTR * img_ptr[1];
+  if (subpix_y > 0) *patch_ptr += wBL * img_ptr[refStep];
+  if (subpix_x > 0 && subpix_y > 0) *patch_ptr += wBR * img_ptr[refStep + 1];
 
   // Get pixel coordinates without border (border width = 1)
-  int x_wo_border = x - 1;
-  int y_wo_border = y - 1;
-  bool within_border = x_wo_border >= 0 && y_wo_border >= 0 &&
-                       x_wo_border < 2 * (halfpatch_size - 1) &&
-                       y_wo_border < 2 * (halfpatch_size - 1);
-
-  if (x < 2 * halfpatch_size && y < 2 * halfpatch_size) {
-    const float dx = x - halfpatch_size + 0.5f;
-    const float dy = y - halfpatch_size + 0.5f;
-
-    const float wdx = warp_c[0][0] * dx + warp_c[0][1] * dy;
-    const float wdy = warp_c[1][0] * dx + warp_c[1][1] * dy;
-
-    const float u_pixel = c_x + wdx - 0.5f;
-    const float v_pixel = c_y + wdy - 0.5f;
-
-    const int u_r = floor(u_pixel);
-    const int v_r = floor(v_pixel);
-
-    const float subpix_x = u_pixel - u_r;
-    const float subpix_y = v_pixel - v_r;
-
-    const float wTL = (1.0f - subpix_x) * (1.0f - subpix_y);
-    const float wTR = subpix_x * (1.0f - subpix_y);
-    const float wBL = (1.0f - subpix_x) * subpix_y;
-    const float wBR = subpix_x * subpix_y;
-
-    const uchar* img_ptr = d_img.data + v_r * refStep + u_r;
-    float* patch_ptr = d_patchWithBorder + y * patchSizeWithBorder + x;
-
-    *patch_ptr = wTL * img_ptr[0];
-    if (subpix_x > 0) *patch_ptr += wTR * img_ptr[1];
-    if (subpix_y > 0) *patch_ptr += wBL * img_ptr[refStep];
-    if (subpix_x > 0 && subpix_y > 0) *patch_ptr += wBR * img_ptr[refStep + 1];
-
-    if (within_border) {
-      float* patch_wo_border_ptr = d_patch + y_wo_border * 2 * halfpatch_size + x_wo_border;
-      *patch_wo_border_ptr = *patch_ptr;
-    }
+  const int x_wo_border = x - 1;
+  const int y_wo_border = y - 1;
+  const bool within_border = x_wo_border >= 0 && y_wo_border >= 0 &&
+                             x_wo_border < 2 * (halfpatch_size - 1) &&
+                             y_wo_border < 2 * (halfpatch_size - 1);
+  if (within_border) {
+    float* patch_wo_border_ptr =
+        d_patch + y_wo_border * 2 * halfpatch_size + x_wo_border;
+    *patch_wo_border_ptr = *patch_ptr;
   }
 }
 
